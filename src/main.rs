e021231d9f4343d715245841970817271ee67f4f
Elie Cohen <ec92009@gmail.com>
2022-10-18 17:03:04 +0100

    init

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,13 @@ fn main() {
     };
 
     println!("Hello, world!, number = {}", number);
+
+    let x = 3i32;
+    let y = 43i32;
+    println!("Hello, world!, x = {}, y = {}, x+y = {}", x, y, add(x, y));
+}
+
+fn add(x: i32, y: i32) -> i32 {
+    x + y
 }
 
